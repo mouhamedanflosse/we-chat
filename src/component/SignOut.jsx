8ce132ac66b9697c7bfabf7bc46784a80ea1734f
mouhamed anflouss <mouhamedanflosse@gmail.com>
2023-08-16 13:02:52 +0100

    change some text

--- src/component/SignOut.jsx
+++ src/component/SignOut.jsx
@@ -13,7 +13,7 @@ const SignOut = () => {
       onClick={Signout}
       className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
         <TbLogout className="text-[20px] mr-1 my-0"/>
-      <span>sign Out</span>
+      <span>Sign Out</span>
       </button>
     </div>
   );
